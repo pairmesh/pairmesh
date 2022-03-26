8853a9ce6305d48cbf002738dced95004c61a8e5
hi-windy <hi.windy.han@gmail.com>
2022-03-26 20:05:48 +0800

    package macos application
    Signed-off-by: hi-windy <hi.windy.han@gmail.com>

--- build/macos_dist.sh
+++ build/macos_dist.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# Prepare packager
+cd tools/appify && go build .
+cd ../..
+
+# Prepare distribution directory
 mkdir dist
+
+# Build and package application
 for arch in "amd64" "arm64"
 do
+  make clean
   GOARCH=$arch make pairmesh
-  tar -czf dist/"pairmesh-$(git describe --tags --abbrev=0)-macos-$arch.tar.gz" -C bin pairmesh
+  tools/appify/appify  -name PairMesh -author "PairMesh.com" -version "$(git describe --always --tags --abbrev=0)" -id com.PairMesh.app -icon ./node/resources/icon_darwin.png bin/pairmesh
+  zip dist/"pairmesh-$(git describe --tags --abbrev=0)-macos-$arch.zip" -r PairMesh.app
+  rm -rf PairMesh.app
 done
