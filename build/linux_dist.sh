d5bbd3c44ee2f23cbd864e4613bad3be7a65f383
hi-windy <hi.windy.han@gmail.com>
2022-03-26 14:12:10 +0800

    add release github action
    Signed-off-by: hi-windy <hi.windy.han@gmail.com>

--- build/linux_dist.sh
+++ build/linux_dist.sh
@@ -4,5 +4,5 @@ mkdir dist
 for arch in "amd64"
 do
   GOARCH=$arch make pairmesh
-  tar -czf dist/"pairmesh_$(git describe --tags --abbrev=0)_linux_$arch.tar.gz" -C bin pairmesh
+  tar -czf dist/"pairmesh-$(git describe --tags --abbrev=0)-linux-$arch.tar.gz" -C bin pairmesh
 done
--- build/macos_dist.sh
+++ build/macos_dist.sh
@@ -4,5 +4,5 @@ mkdir dist
 for arch in "amd64" "arm64"
 do
   GOARCH=$arch make pairmesh
-  tar -czf dist/"pairmesh_$(git describe --tags --abbrev=0)_macos_$arch.tar.gz" -C bin pairmesh
+  tar -czf dist/"pairmesh-$(git describe --tags --abbrev=0)-macos-$arch.tar.gz" -C bin pairmesh
 done
