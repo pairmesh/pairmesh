a1018a27f4a4aa23182d7afc5f03119ee27be764
hi-windy <hi.windy.han@gmail.com>
2022-03-31 11:57:33 +0800

    fix package scripts
    Signed-off-by: hi-windy <hi.windy.han@gmail.com>

--- build/macos_dist.sh
+++ build/macos_dist.sh
@@ -5,14 +5,24 @@ cd tools/appify && go build .
 cd ../..
 
 # Prepare distribution directory
-mkdir dist
+mkdir -p dist
+
+# Remove previous building
+rm -rf PairMesh.app
 
 # Build and package application
 for arch in "amd64" "arm64"
 do
   make clean
+
+  # Build PairMesh entry process binary
+  GO111MODULE=on CGO_ENABLED=1 GOARCH=$arch go build -ldflags "-s -w" -o bin/macos ./app/macos
+  tools/appify/appify  -name PairMesh -author "PairMesh.com" -version "$(git describe --always --tags --abbrev=0)" -id com.PairMesh.app -icon ./node/resources/icon_darwin.png bin/macos
+
+  # Build PairMesh daemon process binary
   GOARCH=$arch make pairmesh
-  tools/appify/appify  -name PairMesh -author "PairMesh.com" -version "$(git describe --always --tags --abbrev=0)" -id com.PairMesh.app -icon ./node/resources/icon_darwin.png bin/pairmesh
+  mkdir PairMesh.app/Contents/Daemon
+  cp bin/pairmesh PairMesh.app/Contents/Daemon
   zip dist/"pairmesh-$(git describe --tags --abbrev=0)-macos-$arch.zip" -r PairMesh.app
   rm -rf PairMesh.app
 done
